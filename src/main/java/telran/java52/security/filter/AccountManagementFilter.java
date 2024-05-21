1ed3ae4aee616ff9102747bd13fd7d582f1078a6
Anton Viltovski <antonviltovski@gmail.com>
2024-05-21 20:24:22 +0300

    Account and admin managment

--- src/main/java/telran/java52/security/filter/AccountManagementFilter.java
+++ src/main/java/telran/java52/security/filter/AccountManagementFilter.java
@@ -2,13 +2,10 @@ package telran.java52.security.filter;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.Enumeration;
-import java.util.Map;
 
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -19,14 +16,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import telran.java52.accounting.dao.UserAccountRepository;
-import telran.java52.accounting.dto.exceptions.IncorrectRoleException;
 import telran.java52.accounting.model.Role;
 import telran.java52.accounting.model.UserAccount;
 
 @Component
 @RequiredArgsConstructor
-@Order(20)
-public class AdminManagingRolesFilter implements Filter {
+@Order(30)
+public class AccountManagementFilter implements Filter {
 	final UserAccountRepository userAccountRepository;
 
 	@Override
@@ -34,28 +30,28 @@ public class AdminManagingRolesFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
-	
-
-		if (checkEndpoint(request.getMethod(), request.getServletPath())) {
+		if(checkEndpoint(request.getMethod(), request.getServletPath())) {
 			try {
 				Principal principal = request.getUserPrincipal();
 				String userName = principal.getName();
-				UserAccount userAccount = userAccountRepository.findById(userName)
-															   .orElseThrow(RuntimeException::new);
-				if (!userAccount.getRoles().contains(Role.ADMINISTRATOR)) {
+				UserAccount userAccount = userAccountRepository.findById(userName).orElseThrow(RuntimeException::new);
+				if(userAccount.getRoles().contains(Role.ADMINISTRATOR) && HttpMethod.PUT.matches(request.getMethod())) {
+					throw new RuntimeException();
+				}								
+				if(!userName.equals(userAccount.getLogin())) {
 					throw new RuntimeException();
 				}
-			} catch (Exception e) {
+			}catch(Exception e){
 				response.sendError(409);
 			}
 		}
-
 		chain.doFilter(request, response);
-	}
 
-	private boolean checkEndpoint(String method, String path) {
-		String rolePatern = "/account/user/.+/role/.+$";
-		return ((HttpMethod.PUT.matches(method) || HttpMethod.DELETE.matches(method)) && path.matches(rolePatern));
+	}
+	
+	private boolean checkEndpoint(String method, String path) {	
+		String pattern = "/account/user/.+$";
+		return ((HttpMethod.PUT.matches(method) || HttpMethod.DELETE.matches(method)) && path.matches(pattern));
 	}
 
 }
